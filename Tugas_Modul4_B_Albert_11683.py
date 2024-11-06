9a078a07d1d4f0c5d80eddce4d17f59a905f1455
AlbertCorn <220711683@students.uajy.ac.id>
2024-11-06 16:52:42 +0700

    Update Tugas_Modul4_B_Albert_11683.py

--- Tugas_Modul4_B_Albert_11683.py
+++ Tugas_Modul4_B_Albert_11683.py
@@ -47,11 +47,10 @@ if uploaded_file is not None:
     st.markdown("<h1 style='text-align: centerl'>Unsupervised Learning - 11683</h1>", unsafe_allow_html=True)
     st.dataframe(input_data)
     
-    model_directory = r'D:\Semester 5\Pembelajaran mesin dan Pembelajaran Mendalam\MOdul unsupervised\Tugas4_B_11683'
     model_path = {
-        "AGG_model" : os.path.join(model_directory, r'AGG_model.pkl'),
-        "KMeans_model" : os.path.join(model_directory, r'KMeans_model.pkl'),
-        "DBSCAN_model" : os.path.join(model_directory, r'DBSCAN_model.pkl')
+        "AGG_model" : r'AGG_model.pkl',
+        "KMeans_model" : r'KMeans_model.pkl',
+        "DBSCAN_model" : r'DBSCAN_model.pkl'
     }
     
     models = {}
